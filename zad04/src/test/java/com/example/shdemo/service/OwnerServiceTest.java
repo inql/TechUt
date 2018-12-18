1f66f472f3eb8e07a01d4c98385019c91e8d6144
inql <dbinkus96@gmail.com>
2018-12-18 21:28:56 +0100

    implement addDogToOwner test

--- zad04/src/test/java/com/example/shdemo/service/OwnerServiceTest.java
+++ zad04/src/test/java/com/example/shdemo/service/OwnerServiceTest.java
@@ -34,11 +34,6 @@ public class OwnerServiceTest {
     private final Double DOG1_WEIGHT = 23.5;
     private final Character DOG1_SEX = 'c';
 
-    private final String DOG2_NAME = "Reksio";
-    private final Boolean DOG2_IS_VACCINATED = false;
-    private final Double DOG2_WEIGHT = 4.9;
-    private final Character DOG2_SEX = 'c';
-
     private final String OWNER_FNAME = "Jan";
     private final String OWNER_LNAME = "Abacki";
     private final Date OWNER_BDATE = new GregorianCalendar(100,10,10).getTime();
@@ -88,6 +83,52 @@ public class OwnerServiceTest {
         assertEquals(OWNER_FNAME,addedOwner.getFirstName());
         assertEquals("Babacki",addedOwner.getLastName());
         assertEquals(OWNER_BDATE,addedOwner.getBirthDate());
+    }
+
+    @Test
+    public void addDogToOwnerTest(){
+        List<Dog> dogList = dogService.getAllDogs();
+
+        for (Dog dog: dogList){
+            if(dog.getName().equals(DOG1_NAME)){
+                dogService.deleteDog(dog);
+            }
+        }
+
+        Dog dogToAdd = new Dog();
+        dogToAdd.setName(DOG1_NAME);
+        dogToAdd.setVaccinated(DOG1_IS_VACCINATED);
+        dogToAdd.setWeight(DOG1_WEIGHT);
+        dogToAdd.setSex(DOG1_SEX);
+
+
+        List<Owner> owners = ownerService.getAllOwners();
+
+        for(Owner owner : owners){
+            if(owner.getFirstName().equals(OWNER_FNAME))
+                ownerService.deleteOwner(owner);
+        }
+
+        Owner ownerToAdd = new Owner();
+        ownerToAdd.setFirstName(OWNER_FNAME);
+        ownerToAdd.setLastName(OWNER_LNAME);
+        ownerToAdd.setBirthDate(OWNER_BDATE);
+        ownerToAdd.getDogList().add(dogToAdd);
+
+        dogService.addDog(dogToAdd);
+        ownerService.addOwner(ownerToAdd);
+        Owner addedOwner = ownerService.getOwnerByName(OWNER_FNAME);
+        Dog ownersDog = addedOwner.getDogList().get(0);
+
+        assertEquals(OWNER_FNAME,addedOwner.getFirstName());
+        assertEquals(OWNER_LNAME,addedOwner.getLastName());
+        assertEquals(OWNER_BDATE,addedOwner.getBirthDate());
+        assertEquals(1,addedOwner.getDogList().size());
+        assertEquals(DOG1_NAME,ownersDog.getName());
+        assertEquals(DOG1_WEIGHT,ownersDog.getWeight());
+        assertEquals(DOG1_SEX,ownersDog.getSex());
+        assertEquals(DOG1_IS_VACCINATED,ownersDog.getVaccinated());
+
     }
 
     @Test
